d7867c0c8559670881750823469345bd698add36
Solomon Kassaye <nomo5503@gmail.com>
2017-07-05 10:39:57 -0400

    Revert "third stage"

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -2,69 +2,51 @@ import React, { Component } from 'react';
 import './Project.css';
 
 class Project extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isVideoShown: false
-    }
-    this.toggleVideo = this.toggleVideo.bind(this);
-    this.closeVideo = this.closeVideo.bind(this);
-  }
-  toggleVideo() {
-    console.log('clicked')
-    this.setState({isVideoShown: !this.state.isVideoShown});
-    this.refs.video.play();
-  }
-  closeVideo() {
-    console.log('clicked hide')
-    this.setState({isVideoShown: false});
-    this.refs.video.pause();
-  }
+
   render(props) {
     const vSrc = this.props.videoSrc;
     const iSrc = this.props.thumbSrc;
-    const proName = this.props.projectName;
-    const tags = this.props.projectTags;
-    const techs = this.props.projectTechs;
-
-    const techsList = techs.map((tech, index) => {
-      return <li key={index}><a href="void(0)">{tech}</a></li>
-    });
-    const tagsList = tags.map((tag, index) => {
-      return <li key={index}><a href="void(0)">#{tag}</a></li>
-    });
     return (
       <div className="sk-project">
         <div className="sk-project-card">
-          <div className="project-thumbnail" onClick={this.toggleVideo}>
-            <img src={iSrc} alt="thumbnail"/>
+          
+          <div className="project-thumbnail">
+            <img src={iSrc} alt=""/>
             <span className="hover-bg"></span>
-            <span className="play-icon"></span>
           </div>
           <div className="project-info">
-            <a href="void(0)" className="project-name">{proName}</a>
+            <a href="void(0)" className="project-name">SunShot - Building DataCloud Application UI Prototype </a>
             <div className="project-technologies">
               <p>Technologies :</p>
               <ul>
-                {techsList}
+                <li><a href="void(0)">Html5</a></li>
+                <li><a href="void(0)">Javascript</a></li>
+                <li><a href="void(0)">Css3</a></li>
               </ul>
             </div>
             <div className="project-technologies project-tags">
               <p>Tags :</p>
               <ul>
-                {tagsList}
+                <li><a href="void(0)">Login</a></li>
+                <li><a href="void(0)">Validation Error</a></li>
+                <li><a href="void(0)">Search</a></li>
+                <li><a href="void(0)">Sidebar</a></li>
+                <li><a href="void(0)">Tabs</a></li>
+                <li><a href="void(0)">Google Map</a></li>
+                <li><a href="void(0)">Expandable Row</a></li>
+                <li><a href="void(0)">Filter</a></li>
+                <li><a href="void(0)">Modal</a></li>
+                <li><a href="void(0)">Progress Bar</a></li>
+                <li><a href="void(0)">Search</a></li>
               </ul>
             </div>
           </div>
         </div>
-        <div className={"video-overlay " + (this.state.isVideoShown ? 'shown' : '')}>
-          <div className="video-container">
-            <video preload controls muted ref="video">
-              <source src={vSrc} type="video/mp4"></source>
-            </video>
-          </div>
-          <div className="video-bg-cover" onClick={this.closeVideo}></div>
-          <div className="close-video-btn" onClick={this.closeVideo}><span></span><span></span></div>
+        <div className="video-container hidden">
+          <div className="bg-cover"></div>
+          <video autoPlay preload controls muted>
+            <source src={vSrc} type="video/mp4"></source>
+          </video>
         </div>
       </div>
     );
